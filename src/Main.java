25525628754b255277ec74c096d51f30cff6182c
NaushinMaliha123 <tiashamaliha@gmail.com>
2024-05-02 13:38:34 +0600

    comitted

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+        System.out.println();
+        person.setAge(21);
+        person.setName("Chandon");
+        System.out.println("Output:");
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
+
+}
